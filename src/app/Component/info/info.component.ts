263c49daa10293f6df68206afb6c07cbfd29427d
mo7med mnz <mohmedmnz74@gmail.com>
2024-01-27 19:25:40 +0200

    some  changes in contact us info and engineer

--- src/app/Component/info/info.component.ts
+++ src/app/Component/info/info.component.ts
@@ -1,43 +1,47 @@
-import { Component } from '@angular/core';
-
-
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(1000, style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
 
+  showAnimation = false;
 
+  constructor(private el: ElementRef) {}
 
   images = [
     { url: './assets/images/1.webp', category: 'تركيب مظلات' },
-        { url: './assets/images/2.webp', category: 'تركيب مظلات' },
+    { url: './assets/images/2.webp', category: 'تركيب مظلات' },
     { url: './assets/images/3.webp', category: 'تركيب مظلات' },
     { url: './assets/images/4.webp', category: 'تركيب مظلات' },
-
-
     { url: './assets/images/5.webp', category: 'تنسيق حدائق' },
     { url: './assets/images/6.webp', category: 'تنسيق حدائق' },
     { url: './assets/images/7.webp', category: 'تنسيق حدائق' },
     { url: './assets/images/8.webp', category: 'تنسيق حدائق' },
-
-
-    { url: './assets/images/9.webp', category: ' شلالات' },
-    { url: './assets/images/10.webp', category: ' شلالات'},
-    { url: './assets/images/11.webp', category: ' شلالات' },
-    { url: './assets/images/12.webp', category: ' شلالات' },
-
-
-    { url: './assets/images/13.jpg', category: ' تشطيبات' },
-    { url: './assets/images/14.jpg', category: ' تشطيبات' },
-    { url: './assets/images/15.jpg', category: ' تشطيبات' },
-    { url: './assets/images/16.jpg', category: ' تشطيبات' },
-
+    { url: './assets/images/9.webp', category: 'شلالات' },
+    { url: './assets/images/10.webp', category: 'شلالات' },
+    { url: './assets/images/11.webp', category: 'شلالات' },
+    { url: './assets/images/12.webp', category: 'شلالات' },
+    { url: './assets/images/13.jpg', category: 'تشطيبات' },
+    { url: './assets/images/14.jpg', category: 'تشطيبات' },
+    { url: './assets/images/15.jpg', category: 'تشطيبات' },
+    { url: './assets/images/16.jpg', category: 'تشطيبات' },
   ];
-
-
 
   selectedCategory = 'الجميع';
   filteredImages = this.images; // Initially, show all images
@@ -71,5 +75,35 @@ export class InfoComponent {
     const categoriesSet = new Set<string>();
     this.images.forEach(image => categoriesSet.add(image.category));
     return ['الجميع', ...Array.from(categoriesSet)];
+  }
+
+  ngOnInit() {
+    this.checkScroll();
+
+    // Automatically switch images every 3 seconds
+    setInterval(() => {
+      if (this.showAnimation) {
+        this.paginate((this.currentPage % this.totalPages) + 1);
+      }
+    }, 3000);
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.checkScroll();
+  }
+
+  checkScroll(): void {
+    const element = this.el.nativeElement;
+    const componentPosition = element.getBoundingClientRect().top;
+    const scrollPosition = window.scrollY !== undefined ? window.scrollY : window.innerHeight;
+
+    console.log('Scroll Position:', scrollPosition);
+    console.log('Component Position:', componentPosition);
+
+    if (scrollPosition > componentPosition - window.innerHeight + 300 && !this.showAnimation) {
+      this.showAnimation = true;
+      console.log('Animation Triggered');
+    }
   }
 }
