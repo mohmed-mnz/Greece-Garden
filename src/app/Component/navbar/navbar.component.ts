d8aa074df846e6650b9ec10c1430997d03586698
mo7med mnz <mohmedmnz74@gmail.com>
2024-03-09 19:49:25 +0200

    V5

--- src/app/Component/navbar/navbar.component.ts
+++ src/app/Component/navbar/navbar.component.ts
@@ -1,18 +1,35 @@
-// navbar.component.ts
-
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {}
+  activeSection: string = '';
 
-  isActive(route: string): boolean {
-    return this.route.snapshot.routeConfig?.path === route;
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setActiveSection();
+      }
+    });
+  }
+
+  setActiveSection(): void {
+    const currentRoute = this.router.url.split('#')[1];
+    this.activeSection = currentRoute || 'home';
+  }
+
+  scrollTo(elementId: string, event: Event): void {
+    event.preventDefault();
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
   }
 }
